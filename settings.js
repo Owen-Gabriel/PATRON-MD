8b10f2e95f8483172ac0caa6ab6adc7a1a6339e2
Owen Gabriel <97639957+Owen-Gabriel@users.noreply.github.com>
2025-04-25 20:18:24 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 //auto presence update
-global.bankname = "MONIEPOINT"
+global.bankname = "Access Bank"
 
-global.banknumber = "8133729715"
+global.banknumber = "1855001149"
 
-global.bankowner = "FADARE"
+global.bankowner = "Gabriel Temiloluwa Olaomo"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
